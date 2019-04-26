571ca1f1dc82eb6ba6692f8b6214660e498d1946
Nikolai Huckle <n.huckle@posteo.de>
2019-04-26 22:27:14 +0200

    Some comments and general cleaning

--- omxplay.py
+++ omxplay.py
@@ -11,46 +11,49 @@ pin = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Read the videos and sort them
 videos = sorted(glob.glob("videos/*.mp4"))
 
 #syscmd("export DISPLAY=:0", waiting=True)
 GPIO.add_event_detect(pin, GPIO.BOTH, bouncetime=2000)
 
 while True:
-    # play first video and measure time
+    # play first video in loop and measure time
     t = time.time()
-    syscmd("omxplayer -b --loop --no-osd " + videos[0])
-    print("First video is playing")
+    syscmd("omxplayer -b --loop --no-osd -o local " + videos[0])
+    # Idle till event is detected
     while GPIO.event_detected(pin) == False:
         time.sleep(0.5)
 
-    # Buffer the time
+    # If the video is still playing-->idle some more
     sleepTime = (time.time() - t) % (4 * 60)
     print(sleepTime)
     if sleepTime <= 21:
-        print("went into sleeptime if")
         time.sleep(21 - sleepTime)
     # kill the first video
     syscmd("killall omxplayer.bin", waiting=True)
 
-    # Second video playing
+    # play second video in loop and measure time
     t = time.time()
-    syscmd("omxplayer -b --loop --no-osd " + videos[1])
-    print("second video is playing")
+    syscmd("omxplayer -b --loop --no-osd -o local " + videos[1])
+    # Set event detector hard coded
     wasevent = False
     while (wasevent == False) and ((time.time() - t) < (20 * 3)):
         wasevent = GPIO.event_detected(pin)
         time.sleep(0.4)
 
+    # If the video is still playing, idle some more
     sleepTime = (time.time() - t) % (20)
     print(sleepTime)
     if sleepTime <= 20:
         print("went into second sleeptime if")
         time.sleep(20 - sleepTime)
+
+    # If the button was pushed play the last video and kill everything
     syscmd("killall omxplayer.bin")
     if wasevent == True:
         print("Pressed the button quickly enough. Playing the third video")
-        syscmd("omxplayer -b --no-osd " +
+        syscmd("omxplayer -b --no-osd -o local " +
                videos[2], waiting=True)
         syscmd("killall omxplayer.bin")
 
